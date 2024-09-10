416470cb19391da2b23d4094f4a38421bd5dde5e
torusk <shancun.kazuki@gmail.com>
2024-09-10 16:33:48 +0900

    タイトル１行表示微調整

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ const App: React.FC = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-responsive-title font-bold mb-6 text-center leading-tight">
+      <h1 className="text-[2.5vw] sm:text-[2vw] md:text-[1.8vw] lg:text-[1.5vw] font-bold mb-6 text-center whitespace-nowrap overflow-hidden text-ellipsis">
         ８０パターンで英語が止まらない
       </h1>
       {/* ファイルが選択されていない場合、FileSelector を表示 */}
