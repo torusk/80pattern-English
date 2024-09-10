d824f1b577f6ce5b3ed4ffc1862f813955103217
torusk <shancun.kazuki@gmail.com>
2024-09-10 19:10:28 +0900

    タイトル文字数調整８０を半角の80に

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ const App: React.FC = () => {
   return (
     <div className="max-w-4xl mx-auto p-4">
       <h2 className="text-xl font-semibold mb-4">
-        ８０パターンで英語が止まらない
+        80パターンで英語が止まらない
       </h2>
       {/* ファイルが選択されていない場合、FileSelector を表示 */}
       {!selectedFile && (
